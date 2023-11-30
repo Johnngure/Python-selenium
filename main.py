4fe4bed6c4939b634faeeb9b5ddd2c81858ac54b
johnngure <johnwngure@gmail.com>
2023-11-30 19:17:27 +0300

    modified

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ option.binary_location = '/Application/Brave Browser.App/content/linux/Brave Bro
 
 s = Service(chromedriver)
 
-driver = webdriver.chrome(Services=s, option=option)
+driver = webdriver.Chrome(Service=Service)
 
-driver.get("http://google.com")
+driver.get("https://google.com")
 
 time.sleep(10)
 
