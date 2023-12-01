fd0f18860151e73047a000f4f10bdbbcb926fe28
johnngure <johnwngure@gmail.com>
2023-12-01 15:40:17 +0300

    Selenium

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 chromedriver = "/chromedriver"
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 option = webdriver.ChromeOptions()
@@ -13,6 +15,9 @@ driver = webdriver.Chrome()
 
 driver.get("https://google.com")
 
-time.sleep(5)
+input_element = driver.find_element(By.CLASS_NAME,"gLFyf")
+input_element.send_keys("Ngure technologies" + Keys.ENTER)
+
+time.sleep(20)
 
 driver.quit()
