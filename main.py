b6c125aa006252bd7ede68ce8c9ec32f42f50e8c
johnngure <johnwngure@gmail.com>
2023-12-01 12:10:46 +0300

    Selenium

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ option.binary_location = '/Application/Brave Browser.App/content/linux/Brave Bro
 
 s = Service(chromedriver)
 
-driver = webdriver.Chrome(Service=Service)
+driver = webdriver.Chrome()
 
 driver.get("https://google.com")
 
-time.sleep(10)
+time.sleep(5)
 
 driver.quit()
