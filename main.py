ca0a1af8aae79dfd849ed05b5dd2dfb047facdea
johnngure <johnwngure@gmail.com>
2023-12-06 13:04:29 +0300

    Selenium

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from selenium.webdriver.chrome.service import Service
 chromedriver = "/chromedriver"
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import webDriverwait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 option = webdriver.ChromeOptions()
@@ -15,8 +17,13 @@ driver = webdriver.Chrome()
 
 driver.get("https://google.com")
 
+webDrivewait(driver, 5).untill(
+    EC.presence_of_element_located(By.CLASS_NAME,"gLFyf")
+)
+
 input_element = driver.find_element(By.CLASS_NAME,"gLFyf")
 input_element.send_keys("Ngure technologies" + Keys.ENTER)
+
 
 time.sleep(20)
 
